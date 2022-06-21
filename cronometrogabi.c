180cb4238615c68143e3f53da917ae7acb7fbbea
Gabriel S L <98130988+GSL1M4@users.noreply.github.com>
2022-06-21 19:58:44 -0300

    Update cronometrogabi.c

--- cronometrogabi.c
+++ cronometrogabi.c
@@ -3,23 +3,26 @@
 #include <locale.h>
 #include <windows.h>
 
+//Observação: O programa executa normalmente no DEV-C++
+
 int main(){
 	setlocale(LC_ALL, "Portuguese");
 	int seg, min, hr, cont;
 	
+	system("Color 4E");
+	
 	
 for(hr =0; hr < 24; hr++){
 	printf("%d", hr);
-	fflush(stdin);
 	for(min = 0; min < 60; min ++){
 		printf("%d", min);
-		fflush(stdin);
 		for(seg = 0; seg < 60; seg++){
 			printf("%d", seg);
-			fflush(stdin);
 			system("cls");
 			for(cont = 0; cont == 0; cont++){
-				printf("\t\t\t %dhr %dmin %dseg", hr, min ,seg);
+				printf("\t\t\t\t\t----------Cronômetro Gabi----------\n");
+				printf("\t\t\t\t\t\t %dhr-%dmin-%dseg\n", hr, min ,seg);
+				printf("\t\t\t\t-----------------------------------------------\n");
 				Sleep(1000);
 			}
 			
